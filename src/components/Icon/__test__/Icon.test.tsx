eddea0ea34517c9cacc532b7fef65a413604fa43
Dillon Schultz <dillonschultz93@gmail.com>
2024-03-05 16:53:14 -0800

    Create initial Icon component tests

---
+++ src/components/Icon/__test__/Icon.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Icon from '../Icon';
+
+describe('Icon component', () => {
+  it('Should render the icon component', () => {
+    render(<Icon size="16" name="airplay" />);
+    const icon = screen.getByRole('icon');
+    expect(icon).toBeInTheDocument();
+  });
+});
